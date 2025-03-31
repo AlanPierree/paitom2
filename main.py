aeb83625c09848abf72b87c2481ee7e54746dd57
Alan <161715146+AlanPierree@users.noreply.github.com>
2025-03-31 12:05:48 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
-url = "https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
-print(url)
+print("\n---------------------------")
+print(" Extração de URL em Python!")
+print("---------------------------\n")
 
-url_base = url[0:19]
-print(url_base)
+url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
+
+# Separa base e parâmetros
+indice_interrogacao = url.find('?')
+url_base = url[:indice_interrogacao]
+# 1° parâmetro vazio indica que começa do início, 2° parâmetro vazio vai até o índice final
+url_parametros = url[indice_interrogacao+1:]
+print(url_parametros)
+
+# Busca o valor de um parâmetro
+parametro_busca = 'quantidade'
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+indice_e_comercial = url_parametros.find('&', indice_valor)
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
+
+print(valor)
