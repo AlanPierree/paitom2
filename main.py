2296967cb8b08deab037370ffde670793f02784c
Alan <161715146+AlanPierree@users.noreply.github.com>
2025-04-01 08:45:52 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,11 +4,20 @@ print("---------------------------\n")
 
 url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
 
+# Sanitização da URL
+url = url.replace(" ", "")
+# replace substitui o valor de uma string, do primeiro parâmetro, pelo segundo
+
+# Validação da URL
+if url == "":
+    raise ValueError("A URL está vazia")
+
 # Separa base e parâmetros
 indice_interrogacao = url.find('?')
 url_base = url[:indice_interrogacao]
-# 1° parâmetro vazio indica que começa do início, 2° parâmetro vazio vai até o índice final
+# 1° parâmetro vazio indica que começa do início
 url_parametros = url[indice_interrogacao+1:]
+# 2° parâmetro vazio vai até o índice final
 print(url_parametros)
 
 # Busca o valor de um parâmetro
